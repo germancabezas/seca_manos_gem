9398e47f52eaadb6f793b297cd23c7715d351b23
GERMAN CABEZAS <GERMAN.CABEZAS@LIVE.COM>
2015-03-19 18:42:07 -0500

    initial commit

---
+++ lib/seca_manos.rb
@@ -0,0 +1,11 @@
+require "seca_manos/version"
+
+module SecaManos
+  
+  class Secador
+    def secar
+      `say ssssssssss`
+    end
+  end 
+
+end
---
+++ lib/seca_manos/version.rb
@@ -0,0 +1,3 @@
+module SecaManos
+  VERSION = "0.0.1"
+end
